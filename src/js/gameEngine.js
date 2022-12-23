92a0968495cc6e76acc362c62fbfdc3ac8f9900e
Alexander Uzunov <kaialexander.11@outlook.com>
2022-12-23 17:49:23 +0200

    Add game engine

---
+++ src/js/gameEngine.js
@@ -0,0 +1,8 @@
+function start(state, game) {
+    window.requestAnimationFrame(gameLoop.bind(null, state, game));
+}
+
+function gameLoop(state, game){
+    console.log('frame');
+    window.requestAnimationFrame(gameLoop);
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,16 @@
+
 let state = initState();
 let game = initGameObject();
 
+
 game.startScreen.addEventListener('click', (e) => {
-    console.log('start game');
+
+    //console.log('start game'); //=> test line
     game.startScreen.classList.add('hidden');
     game.gameScreen.classList.remove('hidden');
+
+    //Start game
+    //window.requestAnimationFrame(gameLoop);
+    start(state, game);
 });
+
